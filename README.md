aceb7d844c8166f00b47bc15beffb5bfd5c86ed9
Usama (DevilsShadow) <32026099DevilsSilhouette@users.noreply.github.com>
2017-09-28 19:39:20 -0400

    Some changes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Football_fixture_website
-A website that displays the fixtures for a few football competitions and teams.
+# A Football Fixture Website
+A website that displays the fixtures for a few football competitions and teams among other data related to leagues, competitions and teams.
--- leagueTable.js
+++ leagueTable.js
@@ -1,32 +1,63 @@
 // Just testing some stuff right now. This file calls a specific league
 // and lists all of its teams.
 
+var authToken = "09af676d84d5430a8e4702bfc3e99835";
+
 var request = require('request');
 
-function getLeagueTable(foundUsers) {
+var getTableURL = function(id) {
+	return "http://api.football-data.org/v1/competitions/" + id + "/leagueTable"
+};
 
-  var query = "http://api.football-data.org/v1/competitions/445/leagueTable";
+//var query = "http://api.football-data.org/v1/competitions/445/leagueTable";
 
-  request.get(query, function(error, response, body) {
-    if (error) {
-      return error;
-    }
+/*function getLeagueTable(foundUsers) {
 
-    if (response.statusCode === 200) {
-      var fullData = JSON.parse(body);
-      if (fullData.data) {
-        //console.log("Data: ", fullData.data[0].last_name);
-      }
-      foundUsers(fullData.standing);
-    }
+	request.get(query, function(error, response, body) {
+    	if (error) {
+      		return error;
+    	}
 
-  });
- }
+    	if (response.statusCode === 200) {
+      		var fullData = JSON.parse(body);
+      		if (fullData.data) {
+        		//console.log("Data: ", fullData.data[0].last_name);
+      		}
+      		foundUsers(fullData.standing);
+    	}
 
- getLeagueTable(function(allUsers) {
-  for (var i = 0; i < allUsers.length; i++) {
-  	var currentPosition = allUsers[i];
-  	console.log("In position " + currentPosition.position + ": " + currentPosition.teamName + ", with " +
-                currentPosition.points + " points.");
-  }
- });
+  	});
+}*/
+
+for (var i = 444; i <467; i++) {
+
+	request({
+				url : getTableURL(i),
+				headers: {
+					"X-Auth-Token": authToken
+				}
+			},
+			function(error, response, body) {
+				if (error) {
+					return error;
+				}
+				if (response.statusCode === 200) {
+					var fullData = JSON.parse(body);
+					console.log(fullData.leagueCaption);
+				}
+			})
+
+
+
+	/*getLeagueTable(function(allUsers) {
+
+	console.log("League Table for ");
+
+	for (var i = 0; i < allUsers.length; i++) {
+  		var currentPosition = allUsers[i];
+  		console.log("In position " + currentPosition.position + ": " + currentPosition.teamName + ", with " +
+                	currentPosition.points + " points.");
+  	}
+	});*/
+}
+

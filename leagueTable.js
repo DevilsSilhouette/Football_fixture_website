cf5cf5adb1e006b4b26a283aa812fd65910d55c5
Usama (DevilsShadow) <usamazaki@ymail.com>
2017-12-12 19:31:54 -0500

    Using ajax to get data from api

--- leagueTable.js
+++ leagueTable.js
@@ -3,61 +3,19 @@
 
 var authToken = "09af676d84d5430a8e4702bfc3e99835";
 
-var request = require('request');
-
-var getTableURL = function(id) {
-	return "http://api.football-data.org/v1/competitions/" + id + "/leagueTable"
-};
-
-//var query = "http://api.football-data.org/v1/competitions/445/leagueTable";
-
-/*function getLeagueTable(foundUsers) {
-
-	request.get(query, function(error, response, body) {
-    	if (error) {
-      		return error;
-    	}
-
-    	if (response.statusCode === 200) {
-      		var fullData = JSON.parse(body);
-      		if (fullData.data) {
-        		//console.log("Data: ", fullData.data[0].last_name);
-      		}
-      		foundUsers(fullData.standing);
-    	}
-
-  	});
-}*/
-
-for (var i = 444; i <467; i++) {
-
-	request({
-				url : getTableURL(i),
-				headers: {
-					"X-Auth-Token": authToken
-				}
-			},
-			function(error, response, body) {
-				if (error) {
-					return error;
-				}
-				if (response.statusCode === 200) {
-					var fullData = JSON.parse(body);
-					console.log(fullData.leagueCaption);
-				}
-			})
-
-
-
-	/*getLeagueTable(function(allUsers) {
-
-	console.log("League Table for ");
-
-	for (var i = 0; i < allUsers.length; i++) {
-  		var currentPosition = allUsers[i];
-  		console.log("In position " + currentPosition.position + ": " + currentPosition.teamName + ", with " +
-                	currentPosition.points + " points.");
-  	}
-	});*/
-}
-
+$.ajax({
+	type: 'GET',
+	url: 'http://api.football-data.org/v1/competitions',
+	headers: {"X-Auth-Token": authToken}, 
+	success: function(data) {
+		$.each(data, function(i, league) {
+			let name = league.caption;
+			let row = $('<tr>').text(name);
+			console.log(row);
+			$(row).appendTo('.leagues');
+		});
+	},
+	error: function(err) {
+		console.log("Error: ", err);
+	}
+})

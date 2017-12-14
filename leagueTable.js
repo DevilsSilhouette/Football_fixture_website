6cfb8d1a5943a61976480582c5f040069c94f2ce
Usama (DevilsShadow) <usamazaki@ymail.com>
2017-12-13 22:09:30 -0500

    Page produces the table for one specific leauge.

--- leagueTable.js
+++ leagueTable.js
@@ -5,17 +5,39 @@ var authToken = "09af676d84d5430a8e4702bfc3e99835";
 
 $.ajax({
 	type: 'GET',
-	url: 'http://api.football-data.org/v1/competitions',
+	url: 'http://api.football-data.org/v1/competitions/445/leagueTable',
 	headers: {"X-Auth-Token": authToken}, 
 	success: function(data) {
-		$.each(data, function(i, league) {
-			let name = league.caption;
-			let row = $('<tr>').text(name);
-			console.log(row);
-			$(row).appendTo('.leagues');
+		$('.leagueName').text(data.leagueCaption + " Standings");
+		$.each(data.standing, function(i, team) {
+			let name = team.teamName;
+			let className = convertToClassName(name);
+			let row = $('<tr>').addClass(className);
+
+			if (team.position == 1) {
+				$(row).addClass("table-success");
+			} else if ([18, 19, 20].indexOf(team.position) != -1) {
+				$(row).addClass("table-danger");
+			}
+
+			$('<th>').text(team.position).appendTo(row);
+			$('<td>').text(name).appendTo(row);
+			$('<td>').text(team.playedGames).appendTo(row);
+			$('<td>').text(team.wins).appendTo(row);
+			$('<td>').text(team.draws).appendTo(row);
+			$('<td>').text(team.losses).appendTo(row);
+			$('<td>').text(team.points).appendTo(row);
+			$('<td>').text(team.goals).appendTo(row);
+			$('<td>').text(team.goalsAgainst).appendTo(row);
+			$('<td>').text(team.goalDifference).appendTo(row);
+			$(row).appendTo('.table-body');
 		});
 	},
 	error: function(err) {
 		console.log("Error: ", err);
 	}
 })
+
+let convertToClassName = function(name) {
+	return name.toLowerCase().replace(/ /g, "-");
+}
